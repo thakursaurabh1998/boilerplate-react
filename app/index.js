cb281b8e6d914f172b6a5f1b5b949c90929b82c9
Saurabh Thakur <thakursaurabh1998@gmail.com>
2018-10-08 23:59:34 +0530

    webpack config

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+class App extends Component {
+  render() {
+    return <div>Hello World! This is Saurabh Thakur</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
